37f45f6381cd379cb2a2e4b5bef38979256724b2
Payton Gray <paytongray18@gmail.com>
2020-07-23 15:37:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,3 @@
-def square_array(numbers)
-  num = []
-  counter = 0
-  while counter < number.length do
-    num.push(numbers[counter]**2)
-  end
-end
-
 array = [1,2,3,4]
 def square_array(array)
   new_array = []
