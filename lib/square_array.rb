d394523e93479e63a9c11293b52913e3f7b5da3f
Payton Gray <paytongray18@gmail.com>
2020-07-23 15:36:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,3 +6,13 @@ def square_array(numbers)
   end
 end
 
+array = [1,2,3,4]
+def square_array(array)
+  new_array = []
+  count = 0
+  while count < array.length do
+ squared = new_array.push(array[count]**2)
+    count += 1
+  end
+Return new_array
+end
